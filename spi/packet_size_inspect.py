d5d2c330d57f8a1609a166b712af5b74a7b1673a
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-08-12 16:15:46 +0000

    psi

---
+++ spi/packet_size_inspect.py
@@ -0,0 +1,72 @@
+# ./pox.py misc.full_payload forwarding.l2_learning packet_size_inspect samples.pretty_log log.level --DEBUG info.packet_dump
+
+from pox.core import core
+import pox.lib.packet as pkt
+import pox.openflow.libopenflow_01 as of
+from pox.lib.packet import tcp
+from pox.lib.addresses import IPAddr, EthAddr
+from datetime import datetime
+
+# create a dictionary to put suspicious MAC addresses and threshold
+packets = dict()
+
+def PSI(event):
+    sus_packet_sizes = [145, 250, 238]
+
+    # search for TCP packets
+    SMB_packet = event.parsed.find('tcp')
+    
+    if SMB_packet is None:
+        return
+    # check if its destination port is SMB port
+    elif SMB_packet.dstport == 445:
+        # parse the packet
+        SMB_packet = event.ofp
+        packet = event.parsed
+        
+        # get the size of the packet
+        packet_size = len(SMB_packet)
+        
+        # if the packet size is suspicious, add MAC address to dictionary and increament threshold
+        # then forward the packet to the forwarding.l2_learning component
+        if packet_size in sus_packet_sizes:
+            packets[packet.src] = packets.get(packet.src, 0) + 1
+            print(packet.src, "has sent a SMB suspicious packets")
+            
+            msg = of.ofp_packet_out()
+            msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+            msg.data = event.ofp
+            msg.in_port = event.port
+            
+            for connection in core.openflow.connections:
+                connection.send(msg)
+            event.halt = True
+
+            # if threshold is more than 5, install a rule to block the packet source MAC address from communicating within the network
+            if packets[packet.src] > 5:
+                msg = of.ofp_flow_mod()
+                msg.match.dl_src = packet.src
+                msg.idle_timeout = 1800
+                msg.hard_timeout = 1800
+                
+                for connection in core.openflow.connections:
+                    connection.send(msg)
+                    core.getLogger("SMB Monitor").debug("Blocked host with MAC address %s on port %i: Sending three or more suspicious SMB packets", packet.src ,event.port)
+                    detect_time = str(datetime.now())
+                    print("Detection time is : ", detect_time)
+                event.halt = True
+            else:
+                return
+        else: 
+            msg = of.ofp_packet_out()
+            msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+            msg.data = event.ofp
+            msg.in_port = event.port
+            for connection in core.openflow.connections:
+                connection.send(msg)
+            event.halt = True
+    else:
+        return
+    
+def launch():
+    core.openflow.addListenerByName("PacketIn", PSI, priority=10000)
