f69733a00613b6639213e7f9152bb911f992b6a4
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-08-13 19:22:02 +0000

    honeypot improvements

--- network_scan_based/honeypot_monitor.py
+++ network_scan_based/honeypot_monitor.py
@@ -15,11 +15,11 @@ def HPM(event):
 
     # TODO: some of the ports communicate over UDP so write some code for UDP
     # search for TCP packets
-    Received_packet = event.parsed.find('tcp')
-    if Received_packet is None:
+    tcp_packet = event.parsed.find('tcp')
+    if tcp_packet is None:
         return
     # check if destination port is in the listenting ports list
-    elif Received_packet.dstport in listening_ports:
+    elif tcp_packet.dstport in listening_ports:
         IP = event.parsed.find('ipv4')
         ipaddr = IP.dstip
 
@@ -29,58 +29,58 @@ def HPM(event):
             ipaddr = IP.srcip
             
             detection_time = str(datetime.now())
-            print("suspicious connection found ! <-> connection to honeypot from %s to %i. At time: %s", Received_packet.srcport, Received_packet.dstport, detection_time)
+            print("suspicious connection found ! <-> connection to honeypot from %s to %i. At time: %s", tcp_packet.srcport, tcp_packet.dstport, detection_time)
             
             msg = of.ofp_flow_mod()
             msg.match.dl_type = 0x800
             msg.match.nw_src = ipaddr
             msg.match.nw_proto = 6
-            msg.match.tp_dst = Received_packet.dstport
+            msg.match.tp_dst = tcp_packet.dstport
             msg.idle_timeout = 1200
             msg.hard_timeout = 1800
             
             # block the sender from sending any TCP traffic
             for connection in core.openflow.connections:
                 connection.send(msg)
-                core.getLogger("blocker").debug("installing flow for %s with destination port %i", ip_packet.srcip, Received_packet.dstport)
-                core.getLogger("blocker").debug("blocked suspicious traffic %s <-> %s : connection to honeypot", Received_packet.srcport, Received_packet.dstport)
+                core.getLogger("blocker").debug("installing flow for %s with destination port %i", ip_packet.srcip, tcp_packet.dstport)
+                core.getLogger("blocker").debug("blocked suspicious traffic %s <-> %s : connection to honeypot", tcp_packet.srcport, tcp_packet.dstport)
             event.halt = True
 
             # handle HTTP requests
-            if Received_packet.dstport == 80 or Received_packet.dstport == 443:
+            if tcp_packet.dstport == 80 or tcp_packet.dstport == 443:
                 print("A HTTP request to the Honeypot")
             # handle SMB requests
-            elif Received_packet.dstport == 445 or Received_packet.dstport == 139:
+            elif tcp_packet.dstport == 445 or tcp_packet.dstport == 139:
                 print("A SMB request to Honeypot")
             # handle Remote desktop protocol requests
-            elif Received_packet.dstport == 3389:
+            elif tcp_packet.dstport == 3389:
                 print("A RDP request to Honeypot")
             # handle Remote procedure call requests
-            elif Received_packet.dstport == 135:
+            elif tcp_packet.dstport == 135:
                 rint("A RPC request to Honeypot")
             # handle Windows remote management requests
-            elif Received_packet.dstport == 5985 or Received_packet.dstport == 5986:
+            elif tcp_packet.dstport == 5985 or tcp_packet.dstport == 5986:
                 print("A Windows remote management request to Honeypot")
             # handle FTP requests
-            elif Received_packet.dstport == 20 or Received_packet.dstport == 21:
+            elif tcp_packet.dstport == 20 or tcp_packet.dstport == 21:
                 print("A FTP request to Honeypot")
             # handle SQL requests
-            elif Received_packet.dstport == 3306:
+            elif tcp_packet.dstport == 3306:
                 print("A SQL request to the Honeypot")
             # handle Mail server requests
-            elif Received_packet.dstport == 25 or Received_packet.dstport == 587 or Received_packet.dstport == 993:
+            elif tcp_packet.dstport == 25 or tcp_packet.dstport == 587 or tcp_packet.dstport == 993:
                 print("A mail server related request to the Honeypot")
             # handle SSH requests
-            elif Received_packet.dstport == 22:
+            elif tcp_packet.dstport == 22:
                 print("A SSH request to the Honeypot")
             # handle Telnet requests
-            elif Received_packet.dstport == 23:
+            elif tcp_packet.dstport == 23:
                 print("A Telnet request to the Honeypot")
             # handle LDAP requests
-            elif Received_packet.dstport == 389 or Received_packet.dstport == 636:
+            elif tcp_packet.dstport == 389 or tcp_packet.dstport == 636:
                 print("A LDAP request to the Honeypot")
             # handle AD service requests
-            elif Received_packet.dstport == 9389:
+            elif tcp_packet.dstport == 9389:
                 print("Active directory service requests to the Honeypot")
     else:
         return
