5ed9a875ca11a7af48d9d678e480e2b7f9800645
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-08-14 17:39:48 +0000

    dns monitor

--- dpi/dns_monitor.py
+++ dpi/dns_monitor.py
@@ -8,53 +8,41 @@ from pox.lib.packet.arp import arp
 from datetime import datetime
 
 def DNSMON(event):
+    # suspicious urls
+    suspicious_urls = ["www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com"]
 
     # parse the packet
-    packet = event.parsed.find('dns')
+    udp_packet = event.parsed.find('udp')
 
-    if packet is None:
+    if udp_packet is None:
         return
-    elif packet.dstport == 53:
+    # inspect destination port for ports 139 or 445
+    elif udp_packet.dstport == 53:
+        packet = event.parsed.find('dns')
         
-
-    # check if packet is ARP
-    if packet.type == packet.ARP_TYPE:
-        # ignore packet if destinated to 0.0.0.0
-        if packet.payload.protosrc == IPAddr("0.0.0.0"):
-            return
-        # if packet is packet type request
-        if packet.payload.opcode == arp.REQUEST: 
-            # add MAC address to dictonary and increment threshold
-            mydict[packet.src] = mydict.get(packet.src, 0) + 1
-            print(packet.payload.protosrc, "has performed", mydict[packet.src], "unanswered ARP reuqests.")
-            
-            # if threshold is more than provided, install a rule to block the packet source MAC address from communicating within the network
-            if mydict[packet.src] > threshold_value:
-                detection_time = str(datetime.now())
-                print("suspicious arp packets found ! <-> sending three or more suspicious arp packets. At time:", detection_time)
-
-                ip_src = packet.src
-                msg = of.ofp_flow_mod()
-                msg.match.dl_src = packet.src
-                msg.idle_timeout = 1800
-                msg.hard_timeout = 1800
-                
-                for connection in core.openflow.connections:
-                    connection.send(msg)
-                    core.getLogger("ARP Requests Monitor").debug("blocked host with IP %s on port %i for 30 minutes", packet.payload.protosrc, event.port)
-                    event.halt = True
+        ip_request = event.parsed.find('ipv4')
+   
+        if packet is not None and packet.parsed:
+            # check query name in 
+            for query in packet.questions:
+                if query.name in suspicious_urls:
+                    detection_time = str(datetime.now())
+                    print("suspicious url found ! <-> wannacry url accessed. At time:", detection_time)
+                    
+                    msg = of.ofp_flow_mod()
+                    msg.match = of.ofp_match()
+                    msg.idle_timeout = 1800
+                    msg.hard_timeout = 1800
+                    
+                    for connection in core.openflow.connections:
+                        connection.send(msg)
+                        core.getLogger("DNS Requests Monitor").debug("blocked host with IP %s for 30 minutes from communicating in the network", ip_request.srcip)
+                        event.halt = True
+                return
             else:
-                pass
-        # check if packet type is reply
-        elif packet.payload.opcode == arp.REPLY:
-            # decrease one to MAC source address in the dictionary
-            mydict[packet.dst] = mydict.get(packet.dst, 0) - 1
-            print(packet.payload.protodst, "has performed", mydict[packet.dst], "unanswered ARP requests.")
-        else:
-            return
+                return
     else:
         return
 
 def launch():
-    mydict.clear()
     core.openflow.addListenerByName("PacketIn", DNSMON, priority = 10000)
