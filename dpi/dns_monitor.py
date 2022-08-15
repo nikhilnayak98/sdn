6ae2fc6f80ef583d76837c565fdc8d5e6db38e17
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-08-15 00:11:04 +0000

    dns monitor

--- dpi/dns_monitor.py
+++ dpi/dns_monitor.py
@@ -11,17 +11,20 @@ def DNSMON(event):
     # suspicious urls
     suspicious_urls = ["www.iuqerfsodp9ifjaposdfjhgosurijfaewrwergwea.com"]
 
-    # parse the packet
+    # search for UDP packets
     udp_packet = event.parsed.find('udp')
 
+    # if packet is not UDP return it to forwarding.l2_learning component
     if udp_packet is None:
         return
-    # inspect destination port 53
+    # inspect destination port for 53
     elif udp_packet.dstport == 53:
+        # search for DNS specific packets
         packet = event.parsed.find('dns')
         
         ip_request = event.parsed.find('ipv4')
-   
+
+        # check if the packet is DNS specific
         if packet is not None and packet.parsed:
             # check query name in dns packet questions
             for query in packet.questions:
@@ -29,11 +32,13 @@ def DNSMON(event):
                     detection_time = str(datetime.now())
                     print("suspicious url found ! <-> wannacry url accessed. At time:", detection_time)
                     
+                    # modify flow table entries to add the following matching entries
                     msg = of.ofp_flow_mod()
                     msg.match = of.ofp_match()
                     msg.idle_timeout = 1800
                     msg.hard_timeout = 1800
                     
+                    # install a rule to block the host
                     for connection in core.openflow.connections:
                         connection.send(msg)
                         core.getLogger("DNS Requests Monitor").debug("blocked host with IP %s for 30 minutes from communicating in the network", ip_request.srcip)
