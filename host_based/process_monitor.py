0fe6e076308360ac5f2bb58056cfe66b0bf06ea7
Nikhil Nayak <nikhilnayak98@gmail.com>
2022-09-01 19:08:56 +0100

    Update process_monitor.py

--- host_based/process_monitor.py
+++ host_based/process_monitor.py
@@ -7,22 +7,23 @@ from subprocess import Popen, PIPE, check_output
 
 suspicious_process = ["tasksche.exe", "@WanaDecryptor@.exe", "taskse.exe", "taskdl.exe", "mssecsvc.exe"]
 
+# take tasklist output and parse the table into a dictionary
 def get_live_processes():
-	# Takes tasklist output and parses the table into a dict
 	tasks = check_output(['tasklist']).decode('cp866', 'ignore').split("\r\n")
-	p = []
+	processes = []
 	for task in tasks:
 		m = re.match(b'(.*?)\\s+(\\d+)\\s+(\\w+)\\s+(\\w+)\\s+(.*?)\\s.*', task.encode())
 		if m is not None:
-			p.append({"image":m.group(1).decode(), "pid":int(m.group(2).decode())})
-	return(p)
+			processes.append({"image":m.group(1).decode(), "pid":int(m.group(2).decode())})
+	return(processes)
 
 def run():
 	while True:
 		live_processes = get_live_processes()
 		for process in live_processes:
+			# check if the process in suspicious
 			if process['image'] in suspicious_process:
-				print("Killed" + str(process['image']))
+				print("Killed WannaCry Related process: " + str(process['image']))
 				subprocess.Popen("taskkill /F /T /PID %i"%int(process['pid']) , shell=True)
 	time.sleep(0.01)
 
